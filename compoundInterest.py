9f62bef6a079dc6df6b742f0cdee2bb1dd5d3186
Melissa Kruse <Melissa.Kruse@vu.com>
2022-03-03 00:04:30 -0600

    changed spacing

--- compoundInterest.py
+++ compoundInterest.py
@@ -9,17 +9,17 @@ def calculateCompoundInterest():
     client_one_principal = float(input("Principle (amount): "))
     client_one_time =      float(input("Time:               "))
     client_one_rate =      float(input("Rate:               "))
-    print("Compound Interest:  "+str(round(interest(client_one_principal,client_one_rate,client_one_time),1)))
+    print("Compound Interest: "+str(round(interest(client_one_principal,client_one_rate,client_one_time),1)))
     print ("---")
     client_two_principal = float(input("Principle (amount): "))
     client_two_time =      float(input("Time:               "))
     client_two_rate =      float(input("Rate:               "))
-    print("Compound Interest:  "+str(round(interest(client_two_principal,client_two_rate,client_two_time),2)))
+    print("Compound Interest: "+str(round(interest(client_two_principal,client_two_rate,client_two_time),2)))
     print ("---")
     client_three_principal = float(input("Principle (amount): "))
     client_three_time =      float(input("Time:               "))
     client_three_rate =      float(input("Rate:               "))
-    print("Compound Interest:  "+str(round(interest(client_three_principal,client_three_rate,client_three_time),1)))
+    print("Compound Interest: "+str(round(interest(client_three_principal,client_three_rate,client_three_time),1)))
 
     # end assignment
 
